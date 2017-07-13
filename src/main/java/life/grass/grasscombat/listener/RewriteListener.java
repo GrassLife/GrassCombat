8173d1b9b2f55ed17f3cfd2d4b05eb2f7d3ec757
明地えしら <ecilavip@outlook.jp>
2017-07-14 03:45:53 +0900

    [update] 表示周り更新

--- src/main/java/life/grass/grasscombat/listener/RewriteListener.java
+++ src/main/java/life/grass/grasscombat/listener/RewriteListener.java
@@ -24,7 +24,7 @@ public class RewriteListener implements Listener {
         if(json.hasDynamicValue("AttackReach")) lore.add(ChatColor.BLUE + "リーチ: " + formatDouble(json.getDynamicValue("AttackReach").getAsMaskedDouble().orElse(0.0)));
         if(json.hasDynamicValue("AttackInterval")) lore.add(ChatColor.BLUE + "攻撃間隔: " + json.getDynamicValue("AttackInterval").getAsMaskedInteger().orElse(20));
         if(json.hasDynamicValue("KnockBackPower")) lore.add(ChatColor.BLUE + "吹き飛ばし: " + formatDouble(json.getDynamicValue("KnockBackPower").getAsMaskedDouble().orElse(0.0) * 100) + "%");
-        if(json.hasDynamicValue("AttackWidth")) lore.add(ChatColor.BLUE + "巻き込み: " + formatDouble(json.getDynamicValue("AttackWidth").getAsMaskedInteger().orElse(0) * 100) + "%");
+        if(json.hasDynamicValue("AttackWidth")) lore.add(ChatColor.BLUE + "巻き込み: " + formatDouble(json.getDynamicValue("AttackWidth").getAsMaskedDouble().orElse(0.0) * 100) + "%");
         if(json.hasDynamicValue("AttackRange")) lore.add(ChatColor.BLUE + "射程: " + json.getDynamicValue("AttackRange").getAsMaskedInteger().orElse(20));
         event.setLore(lore);
         event.setShowable(true);
@@ -36,10 +36,10 @@ public class RewriteListener implements Listener {
         List<String> lore = event.getLore();
         lore.add("");
         GrassJson json = event.getJson();
-        if(json.hasDynamicValue("Defence")) lore.add(ChatColor.BLUE + "防御: " + json.getDynamicValue("Defence").getAsOriginalInteger().orElse(0));
-        if(json.hasDynamicValue("Protection")) lore.add(ChatColor.BLUE + "保護: " + json.getDynamicValue("Protection").getAsOriginalInteger().orElse(0));
-        if(json.hasDynamicValue("MagicDefence")) lore.add(ChatColor.BLUE + "魔法防御: "+ json.getDynamicValue("MagicDefence").getAsOriginalInteger().orElse(0));
-        if(json.hasDynamicValue("BonusAttackDamage")) lore.add(ChatColor.BLUE + "追加攻撃力: "+ json.getDynamicValue("BonusAttackDamage").getAsOriginalInteger().orElse(0));
+        if(json.hasDynamicValue("Defence")) lore.add(ChatColor.BLUE + "防御: " + json.getDynamicValue("Defence").getAsMaskedInteger().orElse(0));
+        if(json.hasDynamicValue("Protection")) lore.add(ChatColor.BLUE + "保護: " + json.getDynamicValue("Protection").getAsMaskedInteger().orElse(0));
+        if(json.hasDynamicValue("MagicDefence")) lore.add(ChatColor.BLUE + "魔法防御: "+ json.getDynamicValue("MagicDefence").getAsMaskedInteger().orElse(0));
+        if(json.hasDynamicValue("BonusAttackDamage")) lore.add(ChatColor.BLUE + "追加攻撃力: "+ json.getDynamicValue("BonusAttackDamage").getAsMaskedDouble().orElse(0.0));
         event.setLore(lore);
         event.setShowable(true);
     }
