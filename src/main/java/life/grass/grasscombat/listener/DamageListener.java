5023214dbd040421a8c97b704e29187d8c2a058a
明地えしら <ecilavip@outlook.jp>
2017-07-18 11:16:43 +0900

    [add] スリップダメージが保護で低減できるように

--- src/main/java/life/grass/grasscombat/listener/DamageListener.java
+++ src/main/java/life/grass/grasscombat/listener/DamageListener.java
@@ -20,30 +20,39 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 public class DamageListener implements Listener {
     @EventHandler
     public void onInteract(PlayerInteractEvent e) {
-        if(e.getAction().equals(Action.LEFT_CLICK_AIR) || e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
+        if (e.getAction().equals(Action.LEFT_CLICK_AIR) || e.getAction().equals(Action.LEFT_CLICK_BLOCK)) {
             damageEntity(e.getPlayer());
         }
     }
 
     @EventHandler
     public void onDamageByEntity(EntityDamageByEntityEvent e) {
-        if(e.getDamager() instanceof Player) {
-            if(e.getCause().equals(DamageCause.ENTITY_ATTACK) && e.getDamager() instanceof LivingEntity) {
+        if (e.getDamager() instanceof Player) {
+            if (e.getCause().equals(DamageCause.ENTITY_ATTACK) && e.getDamager() instanceof LivingEntity) {
                 damageEntity((Player) e.getDamager());
                 e.setCancelled(true);
             }
-        } else if(e.getEntity() instanceof LivingEntity) {
+        }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent e) {
+        if (e.getEntity() instanceof LivingEntity) {
             double damage = e.getDamage();
+            System.out.println(e.getCause());
             DressedEntity de = new DressedEntity((LivingEntity) e.getEntity());
-            if(e.getCause().equals(DamageCause.MAGIC) || e.getCause().equals(DamageCause.POISON)) {
+            if (e.getCause().equals(DamageCause.MAGIC)) {
                 damage = DamageUtil.getDefencedDamage(damage, de.getArmorData(ArmorDataType.MAGIC_DEFENCE), de.getArmorData(ArmorDataType.PROTECTION));
-            } else if(e.getCause().equals(DamageCause.ENTITY_ATTACK) || e.getCause().equals(DamageCause.ENTITY_EXPLOSION)) {
+            } else if (e.getCause().equals(DamageCause.POISON) || e.getCause().equals(DamageCause.WITHER)) {
+                damage = DamageUtil.getDefencedDamage(damage, 0.0, de.getArmorData(ArmorDataType.PROTECTION));
+            } else if (e.getCause().equals(DamageCause.ENTITY_ATTACK) || e.getCause().equals(DamageCause.ENTITY_EXPLOSION)) {
                 damage = DamageUtil.getDefencedDamage(damage, de.getArmorData(ArmorDataType.DEFENCE), de.getArmorData(ArmorDataType.PROTECTION));
             }
             e.setDamage(EntityDamageEvent.DamageModifier.ARMOR, 0.0);
             e.setDamage(damage);
         }
     }
+
 
     public void damageEntity(LivingEntity damager) {
         Damager caster = new Damager(damager);
